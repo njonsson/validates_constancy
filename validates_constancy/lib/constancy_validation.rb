da80e051fa98b2a709211fb7c2aeee1137070d4a
Nils Jonsson <nils@alumni.rice.edu>
2007-08-13 05:39:45 +0000

    Reorder the inner modules within ConstancyValidation

--- validates_constancy/lib/constancy_validation.rb
+++ validates_constancy/lib/constancy_validation.rb
@@ -5,6 +5,53 @@ module ConstancyValidation
   
   ActiveRecord::Errors.default_error_messages[:constancy] = "can't be changed"
   
+  # The following validation is defined in the class scope of the model that
+  # you're interested in validating. It offers a declarative way of specifying
+  # when the model is valid and when it is not.
+  module ClassMethods
+    
+    # Encapsulates the pattern of wanting to protect one or more model
+    # attributes from being changed after the model object is created. Example:
+    # 
+    #   class Person < ActiveRecord::Base
+    #     
+    #     # Prevent changes to Person#user_name and Person#member_since.
+    #     validates_constancy_of :user_name, :member_since
+    #     
+    #   end
+    # 
+    # This check is performed only on update.
+    # 
+    # Configuration options:
+    # 
+    # [<tt>:message</tt>] A custom error message (default is: “can’t be changed”)
+    # [<tt>:if</tt>] Specifies a method, Proc or string to call to determine if the validation should occur (e.g., <tt>:if => :allow_validation</tt>, or <tt>:if => Proc.new { |user| user.signup_step > 2 }</tt>). The method, Proc or string should return or evaluate to +true+ or +false+.
+    # 
+    # Warning: With associations, validate the constancy of a foreign key, not
+    # the instance variable itself: <tt>validates_constancy_of :invoice_id</tt>
+    # instead of <tt>validates_constancy_of :invoice</tt>.
+    def validates_constancy_of(*attribute_names)
+      options = {:message =>
+                 ActiveRecord::Errors.default_error_messages[:constancy]}
+      options.merge!(attribute_names.pop) if attribute_names.last.kind_of?(Hash)
+      
+      (@constant_attribute_names ||= []).concat attribute_names.collect!(&:to_s)
+      
+      ConstancyValidation::OriginalAttributesCapture.extend self
+      
+      options.merge! :on => :update
+      validates_each(attribute_names, options) do |record, attribute_name, value|
+        unless value ==
+               record.instance_variable_get(:@original_attributes)[attribute_name]
+          record.errors.add attribute_name, options[:message]
+        end
+      end
+      
+      self
+    end
+    
+  end
+  
   class OriginalAttributesCapture #:nodoc:
     
     class << self
@@ -60,55 +107,7 @@ module ConstancyValidation
     
   end
   
-  # The following validation is defined in the class scope of the model that
-  # you're interested in validating. It offers a declarative way of specifying
-  # when the model is valid and when it is not.
-  module ClassMethods
-    
-    # Encapsulates the pattern of wanting to protect one or more model
-    # attributes from being changed after the model object is created. Example:
-    # 
-    #   class Person < ActiveRecord::Base
-    #     
-    #     # Prevent changes to Person#user_name and Person#member_since.
-    #     validates_constancy_of :user_name, :member_since
-    #     
-    #   end
-    # 
-    # This check is performed only on update.
-    # 
-    # Configuration options:
-    # 
-    # [<tt>:message</tt>] A custom error message (default is: “can’t be changed”)
-    # [<tt>:if</tt>] Specifies a method, Proc or string to call to determine if the validation should occur (e.g., <tt>:if => :allow_validation</tt>, or <tt>:if => Proc.new { |user| user.signup_step > 2 }</tt>). The method, Proc or string should return or evaluate to +true+ or +false+.
-    # 
-    # Warning: With associations, validate the constancy of a foreign key, not
-    # the instance variable itself: <tt>validates_constancy_of :invoice_id</tt>
-    # instead of <tt>validates_constancy_of :invoice</tt>.
-    def validates_constancy_of(*attribute_names)
-      options = {:message =>
-                 ActiveRecord::Errors.default_error_messages[:constancy]}
-      options.merge!(attribute_names.pop) if attribute_names.last.kind_of?(Hash)
-      
-      (@constant_attribute_names ||= []).concat attribute_names.collect!(&:to_s)
-      
-      ConstancyValidation::OriginalAttributesCapture.extend self
-      
-      options.merge! :on => :update
-      validates_each(attribute_names, options) do |record, attribute_name, value|
-        unless value ==
-               record.instance_variable_get(:@original_attributes)[attribute_name]
-          record.errors.add attribute_name, options[:message]
-        end
-      end
-      
-      self
-    end
-    
-  end
-  
-  #:nodoc:
-  def self.included(other_module)
+  def self.included(other_module) #:nodoc:
     other_module.extend ClassMethods
   end
   
