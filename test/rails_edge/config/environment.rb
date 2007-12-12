045ba1d3cf89eb3d433937a31bcaf26b0dfbf917
Nils Jonsson <nils@alumni.rice.edu>
2007-12-12 14:57:03 +0000

    Update for Edge Rails (r8376)

--- test/rails_edge/config/environment.rb
+++ test/rails_edge/config/environment.rb
@@ -1,23 +1,28 @@
-# Be sure to restart your server when you modify this file.
+# Be sure to restart your server when you modify this file
 
 # Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.99.0' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.1' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence over those specified here
+  # Settings in config/environments/* take precedence over those specified here.
+  # Application configuration should go into files in config/initializers
+  # -- all .rb files in that directory are automatically loaded.
+  # See Rails::Configuration for more options.
 
-  # Skip frameworks you're not going to use (only works if using vendor/rails)
-  # config.frameworks -= [ :active_resource, :action_mailer ]
+  # Skip frameworks you're not going to use (only works if using vendor/rails).
+  # To use Rails without a database, you must remove the Active Record framework
+  # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Only load the plugins named here, in the order given. By default, all plugins in vendor/plugins are loaded in alphabetical order.
-  # :all can be used as a placeholder for all plugins not explicitly named.
+  # Only load the plugins named here, in the order given. By default, all plugins 
+  # in vendor/plugins are loaded in alphabetical order.
+  # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Add additional load paths for your own custom dirs
@@ -35,9 +40,11 @@ Rails::Initializer.run do |config|
 
   # Your secret key for verifying cookie session data integrity.
   # If you change this key, all old sessions will become invalid!
+  # Make sure the secret is at least 30 characters and all random, 
+  # no regular words or you'll be exposed to dictionary attacks.
   config.action_controller.session = {
     :session_key => '_validatesconstancy_session',
-    :secret      => 'f56b82d4b81b2b79facee10f40763e97'
+    :secret      => 'f0ec90518ccf18e711ce1441eff2af83c72505ce56cd7ff462b1bfb618b9347953b11f7ad88c7bd2d3afd93a88f0d63850588b0a19bf52a1875b2bdaf522a129'
   }
 
   # Use the database for sessions instead of the cookie-based default,
@@ -55,9 +62,4 @@ Rails::Initializer.run do |config|
 
   # Make Active Record use UTC-base instead of local time
   # config.active_record.default_timezone = :utc
-
-  # See Rails::Configuration for more options
-
-  # Application configuration should go into files in config/initializers
-  # -- all .rb files in that directory are automatically loaded
 end
