e6a669c06f806b625eb99424a04838be18dd2dd2
Mark Catley <mark@nexx.co.nz>
2009-09-23 14:14:44 +1200

    Updating to load all the plugins locales

--- lib/validates_constancy.rb
+++ lib/validates_constancy.rb
@@ -9,7 +9,9 @@ end
 ActiveRecord::Base.class_eval { include ConstancyValidation }
 
 if Object.const_defined?(:I18n)
-  I18n.load_path << File.join(File.dirname(__FILE__), '..', 'config', 'locales', 'en.yml')
+  Dir[File.join(File.dirname(__FILE__), '..', 'config', 'locales', '*.yml')].each do |f|
+    I18n.load_path << f
+  end
 else
   ActiveRecord::Errors.default_error_messages[:constancy] = "can't be changed"
 end
